f61cfe75462f46c6dce4b562561d8ef3d05fc8e3
ArtShai <rinatha93@mail.ru>
2022-09-12 00:12:13 +0300

    ref engine

--- brain_games/engine_game.py
+++ brain_games/engine_game.py
@@ -2,30 +2,20 @@
 
 import prompt
 
-ROUNDS = 3
-
-
-def welcome():
-    print('Welcome to the Brain Games!')
-
-
-def get_username():
-    name = prompt.string('May I have your name? ')
-    print('Hello,', name + '!')
-    return name
-
 
 def get_engine_game(games):
-    welcome()
-    username = get_username()
+    print('Welcome to the Brain Games!')
+    username = prompt.string('May I have your name? ')
+    print('Hello,', username + '!')
     print(games.TASK)
-    for i in range(ROUNDS):
+    rounds_count = 3
+    for i in range(rounds_count):
         question, answer = games.get_round()
         print(f'Question: {question}')
         user_answer = prompt.string('Your answer: ')
         if user_answer == answer:
             print('Correct!')
-        else:
+        if user_answer != answer:
             print(
                 f"'{user_answer}' is wrong answer ;(. Correct answer was '{answer}'. "
                 f"Let's try again, {username}!"
